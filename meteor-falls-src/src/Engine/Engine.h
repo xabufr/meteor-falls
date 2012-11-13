f371613546d045fb6889012ac1998f9b759acac5
Bastien <bastien@bastien-EasyNote-TJ75.(none)>
2012-11-13 16:15:44 +0100

    Debut UnitTest + EngineType (enum)

--- meteor-falls-src/src/Engine/Engine.h
+++ meteor-falls-src/src/Engine/Engine.h
@@ -1,10 +1,26 @@
-using namespace std;
+
+#include <iostream>
+
+class EngineMessage;
+
 class Engine
 {
     public:
 
-     virtual void HandleMessage(Engine Message);
-     virtual void Work();
-     virtual void GetType();
+     virtual void handleMessage(const EngineMessage&) = 0;
+     virtual void work() = 0;
+     virtual void getType() = 0;
+
+};
+
+enum engineType
+
+{
+    NetworkEngine = 0,
+    GraphicEngine = 1,
+    GameEngine = 2,
+    SoundEngine = 3,
+    PhysicalEngine = 4,
+    ScriptEngine = 5,
 
 };
