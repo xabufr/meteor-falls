7be033267572bb8885b43595ec6f8039b8166f44
theshark-fb <theshark-fb@hotmail.fr>
2012-11-16 15:11:59 +0100

    Modification de State et StateManager

---
+++ meteor-falls-src/src/State/State.h
@@ -0,0 +1,31 @@
+#ifndef STATE_H
+#define STATE_H
+
+#include "StateManager.h"
+
+class StateManager;
+
+enum ret_code{
+    FINISHED = 0,
+    HIDE = 1,
+    EXIT_PROGRAM = 2
+};
+
+class State
+{
+    public:
+        State(StateManager *p_state_manager);
+        virtual void enter() = 0;
+        virtual void exit() = 0;
+        virtual ret_code work() = 0;
+
+    protected:
+
+    private:
+        void addState(State *p_state);
+        void removeState(State *p_state);
+        StateManager *m_state_manager;
+        std::vector<State*> m_states;
+};
+
+#endif // STATE_H
