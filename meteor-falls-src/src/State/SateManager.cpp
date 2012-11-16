6c68092dd1e10aac903786bd4207f5a748f6f0d6
theshark-fb <theshark-fb@hotmail.fr>
2012-11-16 11:41:12 +0100

    Ajout du StateManager

---
+++ meteor-falls-src/src/State/SateManager.cpp
@@ -0,0 +1,22 @@
+#include "SateManager.h"
+
+void addState(State *p_state){
+    m_states->push_back(*p_state);
+}
+
+void startLoop(){
+
+}
+
+void exit(){
+
+}
+
+void removeState(State *p_state){
+    int i;
+    for(i=0;i<=m_states.size();i++){
+        if(m_states[i] == *p_state){
+            m_states->erase(i);
+        }
+    }
+}
