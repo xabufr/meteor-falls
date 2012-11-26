f675f0c88a51d45f1edc89f7dbb25ac86b61ba44
zequiel <aramel@epsi.fr>
2012-11-26 22:19:42 +0100

    merge

---
+++ GlobalServer/src/Server.h
@@ -0,0 +1,67 @@
+#ifndef SERVER_H
+#define SERVER_H
+#include <iostream>
+#include <map>
+#include <string>
+#include <boost/serialization/serialization.hpp>
+#include <boost/serialization/map.hpp>
+#include <boost/serialization/vector.hpp>
+#include <boost/serialization/string.hpp>
+
+class Server
+{
+	public :
+		Server(std::string id,std::string ip,std::string nom,int nombre_joueurs_max,int nombre_joueurs_connectes,bool passwd,std::string carte_jouee,std::string type_partie,float temps_jeu);
+
+		std::string get_ip_server() {return m_ip;};
+		void set_ip_server(std::string ip) {m_ip = ip;};
+
+		std::string get_id_server() {return m_id;};
+		void set_id_server(std::string Id) {m_id = Id;};
+
+		std::string get_nom() {return m_nom;};
+        void set_nom(std::string nom) {m_nom = nom;};
+
+		int get_nombre_joueurs_max() {return m_nombre_joueurs_max;};
+		void set_nombre_joueurs_max(int nombre_joueurs_max) {m_nombre_joueurs_max = nombre_joueurs_max;};
+
+		int get_nombre_joueurs_connectes() {return m_nombre_joueurs_connectes;};
+		void set_nombre_joueurs_connectes(int nombre_joueurs_connectes) {m_nombre_joueurs_connectes = m_nombre_joueurs_connectes;};
+
+		bool get_passwd() {return m_passwd;};
+		bool set_passwd(bool passwd) {m_passwd = passwd;};
+
+		std::string get_carte_jouee() {return m_carte_jouee;};
+		void set_carte_jouee(std::string carte_jouee) {m_carte_jouee = carte_jouee;};
+
+		std::string get_type_partie() {return m_type_partie;};
+		void set_type_partie(std::string type_partie) {m_type_partie = type_partie;};
+
+		float get_temps_jeu() {return m_temps_jeu;};
+		void set_temps_jeu(float temps_jeu) {m_temps_jeu = temps_jeu;};
+
+	private :
+		std::string m_id;
+		std::string m_ip;
+		std::string m_nom;
+		int m_nombre_joueurs_max;
+		int m_nombre_joueurs_connectes;
+		bool m_passwd;
+		std::string m_carte_jouee;
+		std::string m_type_partie;
+		float m_temps_jeu;
+
+		friend class boost::serialization::access;
+        template<class Archive>
+        void serialize(Archive& ar, const unsigned int version){
+            ar & m_id & m_ip & m_nom & m_nombre_joueurs_max & m_nombre_joueurs_connectes & m_passwd & m_carte_jouee & m_type_partie & m_temps_jeu;
+        }
+
+};
+
+
+#endif // SERVER_H
+/*
+Server :
+Id_Server
+Ip_Server*/
---
+++ GlobalServer/src/ServerGlobalMessage.h
@@ -0,0 +1,26 @@
+#ifndef SERVERGLOBALMESSAGE_H_INCLUDED
+#define SERVERGLOBALMESSAGE_H_INCLUDED
+
+class ServerGlobalMessage
+{
+  public:
+        enum Type{
+            LOGIN,
+            LOGOUT,
+            SERVER_LIST,
+            SERVER_UP,
+            SERVER_DEL
+        } type;
+
+        std::vector<Server> servers;
+        //Player player;
+        std::vector<std::string> news, command;
+  private:
+        friend class boost::serialization::access;
+        template<class Archive>
+        void serialize(Archive& ar, const unsigned int version){
+            ar & type & servers /*& player*/ & news & command;
+        }
+};
+
+#endif // SERVERGLOBALMESSAGE_H_INCLUDED
