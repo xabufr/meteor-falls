09768df32d426774410faf45ebf4e2af6b51b562
Bastien <bastien@bastien-EasyNote-TJ75.(none)>
2013-01-28 08:46:31 +0100

    Construction de la classe batiment

--- GlobalServer/src/Server.cpp
+++ GlobalServer/src/Server.cpp
@@ -1,23 +1,22 @@
 #include "Server.h"
 
-Server::Server(std::string id,std::string ip,std::string nom,std::string m_version,int nombre_joueurs_max,int nombre_joueurs_connectes,bool passwd,std::string carte_jouee,std::string type_partie,float temps_jeu)
+Server::Server(std::string pip,std::string pnom,std::string pversion,int pnombre_joueurs_max,int pnombre_joueurs_connectes,bool ppasswd,std::string pcarte_jouee,std::string ptype_partie,float ptemps_jeu)
 {
-	m_id = id;
-	m_ip = ip;
-	m_nom = nom;
-	m_nombre_joueurs_max = nombre_joueurs_max;
-	m_nombre_joueurs_connectes = nombre_joueurs_connectes;
-	m_passwd = passwd;
-	m_carte_jouee = carte_jouee;
-	m_type_partie = type_partie;
-	m_temps_jeu = temps_jeu;
+	ip = pip;
+	nom = pnom;
+	nombre_joueurs_max = pnombre_joueurs_max;
+	nombre_joueurs_connectes = pnombre_joueurs_connectes;
+	passwd = ppasswd;
+	carte_jouee = pcarte_jouee;
+	type_partie = ptype_partie;
+	temps_jeu = ptemps_jeu;
 }
 
- Server::Server()
+Server::Server()
 {
-    m_nombre_joueurs_max = 0;
-    m_nombre_joueurs_connectes = 0;
-    m_passwd = 0;
-    m_temps_jeu = 0;
+    nombre_joueurs_max = 0;
+    nombre_joueurs_connectes = 0;
+    passwd = 0;
+    temps_jeu = 0;
 }
 
